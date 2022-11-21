56208f833bb714bb8e356b2cc2faa13a52eda217
PraiseImmanuel <praiseimmanuel601@gmail.com>
2022-11-21 18:41:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import CareerForm from "./components/CareerForm";
+import HeroSection from "./components/HeroSection";
+import JobOpenings from "./components/JobOpenings";
+
+import Nav from "./components/Nav";
+import Principles from "./components/Principles";
+import Testimonials from "./components/Testimonials";
+
+const CareerPage = () => {
+    return (
+        <React.Fragment>
+            <Nav />
+            <HeroSection />
+            <Principles />
+            <JobOpenings />
+            <Testimonials />
+            <CareerForm />
+        </React.Fragment>
+    );
+};
+
+export default CareerPage;
